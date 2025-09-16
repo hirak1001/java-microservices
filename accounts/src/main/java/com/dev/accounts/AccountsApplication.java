ca9dd7b2162e6495db29b8d0b6f798d8383695e2
dev <hirak1001@gmail,com>
2025-09-16 17:04:06 -0500

    enhanced documentation of rest api using @schema

--- accounts/src/main/java/com/dev/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/dev/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.dev.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Dev Patel",
+                        email = "imhirakpatel@gmail.com",
+                        url = "https://github.com/hirak1001"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/hirak1001"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "https://github.com/hirak1001"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
