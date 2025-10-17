fe5961a9767056c60c8d86edbc780959ec860e2c
dev <hirak1001@gmail,com>
2025-10-16 21:36:44 -0500

    Add .gitignore file to ignore IDE and build files

---
+++ v2-spring-cloud-config/accounts/src/main/java/com/dev/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.dev.accounts.mapper;
+
+import com.dev.accounts.dto.CustomerDto;
+import com.dev.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
