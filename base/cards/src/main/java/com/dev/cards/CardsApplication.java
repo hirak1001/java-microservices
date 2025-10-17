872cc360cbcc21f69f6606ab36668c1e9120a02b
dev <hirak1001@gmail,com>
2025-10-16 20:34:14 -0500

    Restructure repository to include base folder hierarchy

---
+++ base/cards/src/main/java/com/dev/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package com.dev.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Dev Patel",
+						email = "imhirakpatel@gmail.com",
+						url = "https://github.com/hirak1001"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/hirak1001"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://github.com/hirak1001"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
